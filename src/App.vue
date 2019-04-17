fb5b9a7a0caf01819cf2239c4190eb1d03ce1607
liv <819880950@qq.com>
2019-04-17 22:59:17 +0200

    switch to using Vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ greeting }} world!</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
--- src/index.jsx
+++
@@ -1,3 +0,0 @@
-import { h, render } from 'preact'
-
-render(<div>Hello!</div>, document.getElementById('content'))
