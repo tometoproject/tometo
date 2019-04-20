1fd8cb7c25cf410705d788ee7909f290178a3d5e
liv <819880950@qq.com>
2019-04-20 20:12:21 +0800

    haha

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
-import router from './router'
+import { router } from './router'
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#content',
   router,
   render: h => h(App)
-})
+}).$mount('#content')
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './components/Home.vue'
 
 export const router = new VueRouter({
   mode: 'history',
+  base: __dirname,
   routes: [
     { path: '/', component: Home }
   ]
