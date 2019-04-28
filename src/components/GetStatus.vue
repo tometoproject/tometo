83430376c7377b9ff20a00b283e4aa4b5026bae4
liv <819880950@qq.com>
2019-04-28 19:11:13 +0200

    Implement GetStatus

--- src/components/GetStatus.vue
+++ src/components/GetStatus.vue
@@ -1,6 +1,19 @@
 <template>
   <section>
-    {{ $route.params.id }}
+    <h1 v-if="!isLoaded">Loading...</h1>
+    <div v-else>
+      <div class="columns">
+        <div class="column is-one-quarter">
+          <img src="https://i.imgur.com/8yR89wl.jpg" />
+        </div>
+        <div class="column">
+          <h1 class="subtitle is-1">
+            <span class="has-text-info">{{ this.$data.played.join(' ') }}</span>
+            {{ this.$data.unplayed.join(' ') }}
+          </h1>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -13,7 +26,30 @@ export default {
       played: [],
       src: null,
       jsonLoaded: false,
-      fullyLoaded: false
+      fullyLoaded: false,
+      words: [],
+      interval: null,
+      index: 0
+    }
+  },
+
+  computed: {
+    isLoaded () {
+      return this.$data.jsonLoaded && this.$data.fullyLoaded
+    }
+  },
+
+  methods: {
+    tick () {
+      if (this.isLoaded && this.$data.index < this.$data.words.length) {
+        const cur = this.$data.words[this.$data.index]
+        const time = this.$data.src.currentTime
+        if (time > Number(cur.end)) {
+          this.$data.index += 1
+          const word = this.$data.unplayed.shift()
+          this.$data.played.push(word)
+        }
+      }
     }
   },
 
@@ -24,12 +60,23 @@ export default {
       this.$data.unplayed = res.content.split(' ')
       this.$data.src = new Audio(res.audio)
 
-      return fetch(res.timestamps, {method: 'GET'})
+      this.$data.src.addEventListener('loadeddata', () => {
+        this.$data.fullyLoaded = true
+        this.$data.interval = setInterval(() => this.tick(), 10)
+        this.$data.src.play()
+      })
+
+      return fetch(res.timestamps, {method: 'GET', cache: 'no-store'}) // for now
     })
-    .then(res2 => res2.text())
+    .then(res2 => res2.json())
     .then(res2 => {
-      console.log(res2)
+      this.$data.words = res2
+      this.$data.jsonLoaded = true
     })
+  },
+
+  beforeDestroy () {
+    clearInterval(this.$data.interval)
   }
 }
 </script>
