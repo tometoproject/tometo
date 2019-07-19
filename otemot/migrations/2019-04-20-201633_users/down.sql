34916ef42f707a07764f8a8caf17b00976e4c12f
marisa <marisa@mokou.zone>
2019-07-19 13:40:50 +0200

    Uppercase the other migrations for consistency

--- otemot/migrations/2019-04-20-201633_users/down.sql
+++ otemot/migrations/2019-04-20-201633_users/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-drop table users;
+DROP TABLE users;
--- otemot/migrations/2019-04-27-192256_create_status/down.sql
+++ otemot/migrations/2019-04-27-192256_create_status/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-drop table statuses;
+DROP TABLE statuses;
