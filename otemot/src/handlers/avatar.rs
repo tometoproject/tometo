775d4911f94ba043a204ad3e83a073723f34ea6d
marisa <marisa@mokou.zone>
2019-07-21 19:37:12 +0200

    Prevent users from creating multiple Avatars

--- otemot/src/handlers/avatar.rs
+++ otemot/src/handlers/avatar.rs
@@ -1,7 +1,7 @@
 use actix::Handler;
 use uuid::Uuid;
 use crate::actor::Oa;
-use crate::models::avatar::{CreateAvatar, NewAvatar};
+use crate::models::avatar::{CreateAvatar, NewAvatar, Avatar};
 use crate::messages::NewResourceMsg;
 use crate::errors::ServiceError;
 use crate::models::user::User;
@@ -21,30 +21,43 @@ impl Handler<CreateAvatar> for Oa {
 			.map_err(|_| ServiceError::InternalServerError)?
 			.pop();
 		
-		match user {
-			None => Err(ServiceError::Unauthorized),
-			Some(u) => {
-				let new_id = Uuid::new_v4();
-				let new_avatar = NewAvatar {
-					id: &new_id.to_string(),
-					user_id: u.id,
-					pitch: avatar.pitch,
-					speed: avatar.speed,
-					language: &avatar.language,
-					gender: &avatar.gender,
-				};
+        if user.is_none() {
+            return Err(ServiceError::Unauthorized);
+        }
 
-				diesel::insert_into(avatars)
-					.values(&new_avatar)
-					.execute(conn)
-					.map_err(|_| ServiceError::InternalServerError)?;
-				
-				Ok(NewResourceMsg {
-					status: 200,
-					id: Some(new_id),
-					message: None,
-				})
-			}
-		}
+        let authed_user = user.unwrap();
+
+        let existing_avatar = avatars
+            .filter(user_id.eq(&authed_user.id))
+            .load::<Avatar>(conn)
+            .map_err(|_| ServiceError::InternalServerError)?
+            .pop();
+        
+        if existing_avatar.is_some() {
+            return Err(ServiceError::BadRequest(
+                "You can't create more than one Avatar!".to_string(),
+            ));
+        }
+
+        let new_id = Uuid::new_v4();
+        let new_avatar = NewAvatar {
+            id: &new_id.to_string(),
+            user_id: authed_user.id,
+            pitch: avatar.pitch,
+            speed: avatar.speed,
+            language: &avatar.language,
+            gender: &avatar.gender,
+        };
+
+        diesel::insert_into(avatars)
+            .values(&new_avatar)
+            .execute(conn)
+            .map_err(|_| ServiceError::InternalServerError)?;
+        
+        Ok(NewResourceMsg {
+            status: 200,
+            id: Some(new_id),
+            message: None,
+        })
 	}
 }
