6dcf624c4f5ac1b433dc2ec6ef48b6b4411146af
marisa <marisa@mokou.zone>
2019-08-14 15:56:51 +0200

    Technically this should work?

--- src/components/GetStatus.vue
+++ src/components/GetStatus.vue
@@ -4,7 +4,8 @@
 		<div v-else>
 			<div class="columns">
 				<div class="column is-one-quarter">
-					<img v-bind:src="this.$data.images.pic1" />
+					<img v-if="!isLoud" v-bind:src="this.$data.images.pic1" />
+					<img v-if="isLoud" v-bind:src="this.$data.images.pic2" />
 				</div>
 				<div class="column">
 					<h1 class="subtitle is-1">
@@ -20,7 +21,6 @@
 <script>
 import router from '../router'
 import config from '../../config.json'
-import { createAudioMeter } from '../audioMeter'
 
 export default {
 	name: 'GetStatus',
@@ -42,6 +42,7 @@ export default {
 				pic2: ""
 			},
 			words: [],
+			isLoud: false,
 			interval: null,
 			index: 0
 		}
@@ -58,12 +59,32 @@ export default {
 			if (this.isLoaded && this.$data.index < this.$data.words.length) {
 				const cur = this.$data.words[this.$data.index]
 				const time = this.$data.audio.ctx.currentTime
+				if (this.getVolume() > 1) {
+					this.$data.isLoud = true
+				} else {
+					this.$data.isLoud = false
+				}
 				if (time > Number(cur.begin)) {
 					this.$data.index += 1
 					const word = this.$data.unplayed.shift()
 					this.$data.played.push(word)
 				}
 			}
+		},
+
+		getVolume () {
+			let array = new Uint8Array(this.$data.audio.analyzer.fftSize)
+			this.$data.audio.analyzer.getByteTimeDomainData(array)
+
+			let average = 0
+			for (let i = 0; i < array.length; i++) {
+				let a = Math.abs(array[i] - 128)
+				average += a
+			}
+
+			average /= array.length
+
+			return average
 		}
 	},
 
@@ -84,6 +105,8 @@ export default {
 			this.$data.audio.media.crossOrigin = 'anonymous'
 			this.$data.audio.src = this.$data.audio.ctx.createMediaElementSource(this.$data.audio.media)
 			this.$data.audio.analyzer = this.$data.audio.ctx.createAnalyser()
+			this.$data.audio.analyzer.fftSize = 512
+			this.$data.audio.analyzer.smoothingTimeConstant = 0.9
 			this.$data.audio.src.connect(this.$data.audio.analyzer)
 			this.$data.audio.analyzer.connect(this.$data.audio.ctx.destination)
 			this.$data.images.pic1 = res.pic1
@@ -91,7 +114,7 @@ export default {
 
 			this.$data.audio.media.addEventListener('loadeddata', () => {
 				this.$data.fullyLoaded = true
-				this.$data.interval = setInterval(() => this.tick(), 10)
+				this.$data.interval = setInterval(() => this.tick(), 100)
 				this.$data.audio.media.play()
 			})
 
