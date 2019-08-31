3f665c1eca3dc6086be04a8d5106dfcf294a6d48
marisa <mokou@posteo.de>
2019-08-31 15:46:57 +0200

    Add an avatar preview

--- src/components/CreateAvatar.vue
+++ src/components/CreateAvatar.vue
@@ -41,35 +41,43 @@
       </div>
     </div>
 
-    <fieldset class="field">
-      <label class="label">Closed Mouth image</label>
-      <div class="file">
-        <label class="file-label">
-          <input class="file-input" type="file" accept="image/png, image/jpeg" v-on:change="updatePic(1, $event)" />
-          <span class="file-cta">
-            <span class="file-label">Choose a file...</span>
-          </span>
-          <span class="file-name" v-if="fileExists(1)">
-            {{ fileName(1) }}
-          </span>
-        </label>
-      </div>
-    </fieldset>
+    <div class="columns">
+      <div class="column">
+        <fieldset class="field">
+          <label class="label">Closed Mouth image</label>
+          <div class="file">
+            <label class="file-label">
+              <input class="file-input" type="file" accept="image/png, image/jpeg" v-on:change="updatePic(1, $event)" />
+              <span class="file-cta">
+                <span class="file-label">Choose a file...</span>
+              </span>
+              <span class="file-name" v-if="fileExists(1)">
+                {{ fileName(1) }}
+              </span>
+            </label>
+          </div>
+        </fieldset>
 
-    <fieldset class="field">
-      <label class="label">Open Mouth image</label>
-      <div class="file">
-        <label class="file-label">
-          <input class="file-input" type="file" accept="image/png, image/jpeg" v-on:change="updatePic(2, $event)" />
-          <span class="file-cta">
-            <span class="file-label">Choose a file...</span>
-          </span>
-          <span class="file-name" v-if="fileExists(2)">
-            {{ fileName(2) }}
-          </span>
-        </label>
+        <fieldset class="field">
+          <label class="label">Open Mouth image</label>
+          <div class="file">
+            <label class="file-label">
+              <input class="file-input" type="file" accept="image/png, image/jpeg" v-on:change="updatePic(2, $event)" />
+              <span class="file-cta">
+                <span class="file-label">Choose a file...</span>
+              </span>
+              <span class="file-name" v-if="fileExists(2)">
+                {{ fileName(2) }}
+              </span>
+            </label>
+          </div>
+        </fieldset>
       </div>
-    </fieldset>
+
+      <div class="column" v-if="bothFilesAreEntered">
+        <img :src="imagePreview" />
+      </div>
+    </div>
 
     <button class="button is-info" @click="submitForm" :disabled="loading">Submit</button>
   </section>
@@ -86,12 +94,23 @@
        language: 'en',
        gender: 'f',
        pic1: '',
-       pic2: ''
+       pic2: '',
+       interval: null,
+       cycle: true,
+       imagePreview: '',
      }
    },
    computed: {
      loading () {
        return this.$store.state.loading
+     },
+     bothFilesAreEntered () {
+       if (this.$data.pic1 instanceof File && this.$data.pic2 instanceof File) {
+         this.$data.interval = setInterval(this.picCycle, 1000)
+         return true
+       } else {
+         clearInterval(this.$data.interval)
+       }
      }
    },
    methods: {
@@ -109,6 +128,18 @@
      },
      fileName (num) {
        return this.$data[`pic${String(num)}`].name
+     },
+     picCycle () {
+       let reader = new FileReader()
+       reader.addEventListener('load', () => {
+         this.$data.imagePreview = reader.result
+       })
+       if (this.$data.cycle) {
+         reader.readAsDataURL(this.$data.pic1)
+       } else {
+         reader.readAsDataURL(this.$data.pic2)
+       }
+       this.$data.cycle = !this.$data.cycle
      }
    }
  }
