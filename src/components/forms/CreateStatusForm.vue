a71ebac72d75863b8c891b50dd697822b96fd332
marisa <mokou@posteo.de>
2019-10-23 19:05:41 +0200

    Extract form from CreateStatusPage

---
+++ src/components/forms/CreateStatusForm.vue
@@ -0,0 +1,33 @@
+<template>
+	<form v-on:keyup.enter="submitForm">
+		<fieldset>
+			<label>Content</label>
+			<textarea maxlength="500" type="textarea" v-model="content"></textarea>
+		</fieldset>
+
+		<button @click="submitForm" :disabled="loading">Submit</button>
+	</form>
+</template>
+
+<script>
+export default {
+	name: 'CreateStatusForm',
+	data () {
+		return {
+			content: ''
+		}
+	},
+	computed: {
+		loading () {
+			return this.$store.state.loading
+		}
+	},
+	methods: {
+		submitForm (e) {
+			e.preventDefault()
+			let { content } = this
+			this.$store.dispatch('createStatus', { content })
+		}
+	}
+}
+</script>
--- src/components/pages/CreateStatusPage.vue
+++ src/components/pages/CreateStatusPage.vue
@@ -1,41 +1,22 @@
 <template>
-	<form v-on:keyup.enter="submitForm">
+	<section>
 		<h1>New Status</h1>
-
-		<fieldset>
-			<label>Content</label>
-			<textarea maxlength="500" type="textarea" v-model="content"></textarea>
-		</fieldset>
-
-		<button @click="submitForm" :disabled="loading">Submit</button>
-	</form>
+		<CreateStatusForm />
+	</section>
 </template>
 
 <script>
 import router from '../../router'
+import CreateStatusForm from '../forms/CreateStatusForm.vue'
 
 export default {
 	name: 'CreateStatusPage',
-	data () {
-		return {
-			content: '',
-		}
+	components: {
+		CreateStatusForm
 	},
 	beforeMount () {
 		if (!this.$store.state.username)
 			router.back()
-	},
-	computed: {
-		loading () {
-			return this.$store.state.loading
-		}
-	},
-	methods: {
-		submitForm (e) {
-			e.preventDefault()
-			let { content, pitch } = this
-			this.$store.dispatch('createStatus', { content })
-		}
 	}
 }
 </script>
