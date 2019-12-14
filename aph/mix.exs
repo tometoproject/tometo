5ce5ab221a1699f1cd73cfc1a129fc69b105c12c
marisa <mokou@posteo.de>
2019-12-15 00:18:37 +0100

    Remove now-unused dependencies

--- aph/mix.exs
+++ aph/mix.exs
@@ -41,8 +41,6 @@ defmodule Aph.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:guardian, "~> 2.0"},
-      {:comeonin, "~> 5.1"},
       {:argon2_elixir, "~> 2.1"},
       {:cors_plug, "~> 2.0"},
       {:httpoison, "~> 1.6"},
