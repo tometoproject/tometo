139c6160cd7d442d8e01bf2b1218ca1f1afe1f10
marisa <mokou@posteo.de>
2019-12-15 15:50:19 +0100

    Enable OTP releases

--- aph/config/prod.secret.exs
+++ aph/config/prod.secret.exs
@@ -82,12 +82,4 @@ config :aph, AphWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4001")],
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :aph, AphWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+config :aph, AphWeb.Endpoint, server: true
