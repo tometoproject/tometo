3da191b232dbd56744e4391ec1218e4a4b027087
mokou <mokou@posteo.de>
2020-02-13 16:45:06 +0100

    Upgrade Mix dependencies

--- aph/mix.exs
+++ aph/mix.exs
@@ -33,18 +33,18 @@ defmodule Aph.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
+      {:phoenix, "~> 1.4"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.1"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.3"},
+      {:postgrex, "~> 0.15"},
+      {:gettext, "~> 0.17"},
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.1"},
+      {:argon2_elixir, "~> 2.2"},
       {:cors_plug, "~> 2.0"},
       {:httpoison, "~> 1.6"},
-      {:sentry, "~> 7.0"},
+      {:sentry, "~> 7.2"},
       {:phauxth, "~> 2.3"},
       {:uuid, "~> 1.1"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
