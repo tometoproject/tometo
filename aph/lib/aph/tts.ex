4c9e4d8992b9ee55b7b04a530288628008795489
mokou <mokou@posteo.de>
2020-03-05 14:37:30 +0100

    Fix one final Credo issue

--- aph/lib/aph/tts.ex
+++ aph/lib/aph/tts.ex
@@ -63,76 +63,90 @@ defmodule Aph.TTS do
     File.mkdir_p!("gentts/#{status.id}")
 
     if Application.get_env(:aph, :tts) == "google" do
-      api_key = Application.get_env(:aph, :google_key)
-      lang = @g_lang_map[String.to_atom(av.language)]
-
-      gender_num =
-        cond do
-          # en-US has no Standard-A voice for some reason
-          lang == "en-US" and av.gender == "FEMALE" -> "C"
-          lang == "en-US" and av.gender == "MALE" -> "B"
-          av.gender == "FEMALE" -> "A"
-          true -> "B"
-        end
-
-      body =
-        Jason.encode!(%{
-          input: %{text: status.content},
-          voice: %{languageCode: lang, name: "#{lang}-Standard-#{gender_num}"},
-          audioConfig: %{
-            audioEncoding: "OGG_OPUS",
-            pitch: av.pitch || 0,
-            speakingRate: av.speed || 1.0
-          }
-        })
-
-      headers = [{"content-type", "application/json"}]
-
-      with {:ok, res} <-
-             HTTPoison.post(
-               "https://texttospeech.googleapis.com/v1/text:synthesize?key=#{api_key}",
-               body,
-               headers
-             ),
-           {:ok, json} <- Jason.decode(res.body),
-           {:ok, content} <- Base.decode64(json["audioContent"]),
-           :ok <- File.write("gentts/#{status.id}/temp.ogg", content),
-           :ok <- align(status.id, status.content, av.language) do
-        :ok
-      else
-        {:error, err} -> {:tts_error, err}
-      end
+      synthesize_google(status, av)
     else
-      # Since espeak doesn't accept the same values that the Google TTS api does,
-      # we have to convert them from one scale to another.
-      scale_pitch = (av.pitch + 20) / 40 * 99
-      scale_speed = floor((av.speed - 0.25) / 3.75 * 370.0 + 80.0)
+      synthesize_espeak(status, av)
+    end
+  end
 
-      with {_, 0} <-
-             System.cmd("espeak", [
-               "-p",
-               to_string(scale_pitch),
-               "-s",
-               to_string(scale_speed),
-               "-w",
-               "gentts/#{status.id}/temp.wav",
-               status.content
-             ]),
-           {_, 0} <-
-             System.cmd("ffmpeg", [
-               "-i",
-               "gentts/#{status.id}/temp.wav",
-               "-c:a",
-               "libopus",
-               "-b:a",
-               "96K",
-               "gentts/#{status.id}/temp.ogg"
-             ]),
-           :ok <- align(status.id, status.content, av.language) do
-        :ok
-      else
-        {_error, 1} -> {:tts_error, "espeak failed to create audio!"}
+  @doc """
+  Synthesize TTS with Google Text-to-Speech API.
+  """
+  defp synthesize_google(status, av) do
+    api_key = Application.get_env(:aph, :google_key)
+    lang = @g_lang_map[String.to_atom(av.language)]
+
+    gender_num =
+      cond do
+        # en-US has no Standard-A voice for some reason
+        lang == "en-US" and av.gender == "FEMALE" -> "C"
+        lang == "en-US" and av.gender == "MALE" -> "B"
+        av.gender == "FEMALE" -> "A"
+        true -> "B"
       end
+
+    body =
+      Jason.encode!(%{
+        input: %{text: status.content},
+        voice: %{languageCode: lang, name: "#{lang}-Standard-#{gender_num}"},
+        audioConfig: %{
+          audioEncoding: "OGG_OPUS",
+          pitch: av.pitch || 0,
+          speakingRate: av.speed || 1.0
+        }
+      })
+
+    headers = [{"content-type", "application/json"}]
+
+    with {:ok, res} <-
+           HTTPoison.post(
+             "https://texttospeech.googleapis.com/v1/text:synthesize?key=#{api_key}",
+             body,
+             headers
+           ),
+         {:ok, json} <- Jason.decode(res.body),
+         {:ok, content} <- Base.decode64(json["audioContent"]),
+         :ok <- File.write("gentts/#{status.id}/temp.ogg", content),
+         :ok <- align(status.id, status.content, av.language) do
+      :ok
+    else
+      {:error, err} -> {:tts_error, err}
+    end
+  end
+
+  @doc """
+  Synthesizes TTS using espeak.
+  """
+  defp synthesize_espeak(status, av) do
+    # Since espeak doesn't accept the same values that the Google TTS api does,
+    # we have to convert them from one scale to another.
+    scale_pitch = (av.pitch + 20) / 40 * 99
+    scale_speed = floor((av.speed - 0.25) / 3.75 * 370.0 + 80.0)
+
+    with {_, 0} <-
+           System.cmd("espeak", [
+             "-p",
+             to_string(scale_pitch),
+             "-s",
+             to_string(scale_speed),
+             "-w",
+             "gentts/#{status.id}/temp.wav",
+             status.content
+           ]),
+         {_, 0} <-
+           System.cmd("ffmpeg", [
+             "-i",
+             "gentts/#{status.id}/temp.wav",
+             "-c:a",
+             "libopus",
+             "-b:a",
+             "96K",
+             "gentts/#{status.id}/temp.ogg"
+           ]),
+         :ok <- align(status.id, status.content, av.language) do
+      :ok
+    else
+      {_error, 1} -> {:tts_error, "espeak failed to create audio!"}
     end
   end
 
